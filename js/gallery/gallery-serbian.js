28106f475e5944beb4cf0399c60a1eb8cdc31120
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-25 12:23:53 +0100

    form size and footer size and gallery refactor

--- js/gallery/gallery-serbian.js
+++ js/gallery/gallery-serbian.js
@@ -3,52 +3,95 @@ let gallerySerbian = `
 <div class="container-fluid bg-secondary p-0">
     <!-- Gallery -->
     <div class="row" id="galleryTemplateId2">
-        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0" id="galleryRow1">
             <img data-src="img/1.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
                 alt="Prevoz dva automobila na kamionu kroz šumu" />
 
             <img data-src="img/6.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
                 alt="Prevoz dva automobila na kamionu" />
-        </div>
 
-        <div class="col-lg-4 mb-4 mb-lg-0">
-            <img data-src="img/3.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-                alt="Prikolica i kamion sa strane u toku dana" />
-
-            <img data-src="img/4.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-                alt="Prikolica i kamion sa zadnje strane u toku dana" />
-        </div>
-
-        <div class="col-lg-4 mb-4 mb-lg-0">
-            <img data-src="img/5.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-                alt="Prikolica i kamion sa zadnje strane u toku noći" />
-
-            <img data-src="img/2.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-                alt="Utovar vozila na kamionu tokom noći" />
-        </div>
-
-        <div class="col-lg-4 mb-4 mb-lg-0">
             <img data-src="img/7.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
                 alt="Automobil na platformi prikolice" />
 
             <img data-src="img/8.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
                 alt="Dva automobila na prikolici" />
+
+                <img data-src="img/13.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="Transport radne mašine na prikolici, kamion i prikolica sa strane" />
+
+            <img data-src="img/14.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Transport bagera na prikolici, kamion i prikolica sa strane" />
+                
+            <img data-src="img/19.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="transport kombija i auta izbliza uslikano na prikolici" />
+
+            <img data-src="img/20.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="transport kombija i auta od pozadi uslikano na prikolici" />
+
+            <img data-src="img/33.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy" alt="kamion sa desne strane" />
+
+            <img data-src="img/36.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy" alt="kamion sa leve strane" />
         </div>
 
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div class="col-lg-4 mb-4 mb-lg-0" id="galleryRow2">
+            <img data-src="img/3.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Prikolica i kamion sa strane u toku dana" />
+
+            <img data-src="img/4.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Prikolica i kamion sa zadnje strane u toku dana" />
+                
             <img data-src="img/9.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-                alt="Auto na prikolici sa kamionom u pozadini u vožnji noću" />
+            alt="Auto na prikolici sa kamionom u pozadini u vožnji noću" />
 
             <img data-src="img/10.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-                alt="Auto na prikolici sa kamionom u pozadini u vožnji danju" />
+            alt="Auto na prikolici sa kamionom u pozadini u vožnji danju" />
+
+            <img data-src="img/15.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="transport pickup-a na prikolici" />
+
+            <img data-src="img/16.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="transport pickup-a i auta na prikolici" />
+                
+            <img data-src="img/21.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="transport kombija i auta sa strane uslikano" />
+
+            <img data-src="img/22.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="transport dva auta na prikolici" />
+                
+            <img data-src="img/35.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy" alt="kamion sa prednje strane" />
+
+            <img data-src="img/34.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="prevoz dva automobila na kamionu" />
         </div>
 
-        <div class="col-lg-4 mb-4 mb-lg-0">
+        <div class="col-lg-4 mb-4 mb-lg-0" id="galleryRow3">
+            <img data-src="img/40.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Utovar i istovar kamiona" />
+
+            <img data-src="img/5.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Prikolica i kamion sa zadnje strane u toku noći" />
+
+            <img data-src="img/2.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Utovar vozila na kamionu tokom noći" />
+
+                
             <img data-src="img/11.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-                alt="Slepanje vozila iz šume, kamion i vozilo sa strane" />
+            alt="Slepanje vozila iz šume, kamion i vozilo sa strane" />
 
             <img data-src="img/12.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-                alt="Prikolica sa automobilom parkirana ispred kuće" />
+            alt="Prikolica sa automobilom parkirana ispred kuće" />
+
+            <img data-src="img/17.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="transport auta starijeg godišta na prikolici" />
+
+            <img data-src="img/18.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="transport kombija i auta na prikolici" />
+                
+            <img data-src="img/23.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="transport gradjevinskog materijala na prikolici" />
+
+            <img data-src="img/24.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="utovar gradjevinskog materijala na prikolicu" />
         </div>
     </div>
     <!-- Gallery -->
@@ -56,75 +99,106 @@ let gallerySerbian = `
 <!-- Gallery End -->
 `;
 
-let gallerySecondSerbian = `
 
-<div class="col-lg-4 mb-4 mb-lg-0">
-<img data-src="img/13.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="Transport radne mašine na prikolici, kamion i prikolica sa strane" />
+let gallerySerbianSmallScreen = `
+<!-- Gallery Start -->
+<div class="container-fluid bg-secondary p-0">
+    <!-- Gallery -->
+    <div class="row" id="galleryTemplateId2">
+        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0" id="galleryRow1">
+        <img data-src="img/1.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+        alt="Prevoz dva automobila na kamionu kroz šumu" />
+ 
+        <img data-src="img/3.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Prikolica i kamion sa strane u toku dana" />
+        
+        <img data-src="img/40.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Utovar i istovar kamiona" />
+        
+        <img data-src="img/6.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Prevoz dva automobila na kamionu" />
+        
+        <img data-src="img/4.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Prikolica i kamion sa zadnje strane u toku dana" />
+        
+        <img data-src="img/5.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Prikolica i kamion sa zadnje strane u toku noći" />
+        
+        <img data-src="img/7.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Automobil na platformi prikolice" />
+        
+        <img data-src="img/9.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Auto na prikolici sa kamionom u pozadini u vožnji noću" />
+        
+        <img data-src="img/2.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Utovar vozila na kamionu tokom noći" />
+        
+        <img data-src="img/8.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Dva automobila na prikolici" />
+        
+        <img data-src="img/10.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Auto na prikolici sa kamionom u pozadini u vožnji danju" />
+        
+        <img data-src="img/11.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Slepanje vozila iz šume, kamion i vozilo sa strane" />
+        
+        <img data-src="img/12.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+                alt="Prikolica sa automobilom parkirana ispred kuće" />
 
-<img data-src="img/14.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="Transport bagera na prikolici, kamion i prikolica sa strane" />
+        <img data-src="img/13.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="Transport radne mašine na prikolici, kamion i prikolica sa strane" />
+        
+        <img data-src="img/14.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="Transport bagera na prikolici, kamion i prikolica sa strane" />
+            
+        <img data-src="img/19.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="transport kombija i auta izbliza uslikano na prikolici" />
+        
+        <img data-src="img/20.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="transport kombija i auta od pozadi uslikano na prikolici" />
+        
+        <img data-src="img/33.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy" alt="kamion sa desne strane" />
+        
+        <img data-src="img/36.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy" alt="kamion sa leve strane" />
+
+        <img data-src="img/15.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="transport pickup-a na prikolici" />
+        
+        <img data-src="img/16.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="transport pickup-a i auta na prikolici" />
+            
+        <img data-src="img/21.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="transport kombija i auta sa strane uslikano" />
+        
+        <img data-src="img/22.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="transport dva auta na prikolici" />
+            
+        <img data-src="img/35.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy" alt="kamion sa prednje strane" />
+        
+        <img data-src="img/34.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="prevoz dva automobila na kamionu" />
+
+        <img data-src="img/17.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="transport auta starijeg godišta na prikolici" />
+        
+        <img data-src="img/18.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="transport kombija i auta na prikolici" />
+            
+        <img data-src="img/23.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="transport gradjevinskog materijala na prikolici" />
+        
+        <img data-src="img/24.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
+            alt="utovar gradjevinskog materijala na prikolicu" />
+
+        </div>
+    </div>
+    <!-- Gallery -->
 </div>
-
-<div class="col-lg-4 mb-4 mb-lg-0">
-<img data-src="img/15.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="transport pickup-a na prikolici" />
-
-<img data-src="img/16.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="transport pickup-a i auta na prikolici" />
-</div>
-
-<div class="col-lg-4 mb-4 mb-lg-0">
-<img data-src="img/17.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="transport auta starijeg godišta na prikolici" />
-
-<img data-src="img/18.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="transport kombija i auta na prikolici" />
-</div>
-
-<div class="col-lg-4 mb-4 mb-lg-0">
-<img data-src="img/19.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="transport kombija i auta izbliza uslikano na prikolici" />
-
-<img data-src="img/20.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="transport kombija i auta od pozadi uslikano na prikolici" />
-</div>
-
-<div class="col-lg-4 mb-4 mb-lg-0">
-<img data-src="img/21.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="transport kombija i auta sa strane uslikano" />
-
-<img data-src="img/22.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="transport dva auta na prikolici" />
-</div>
-
-<div class="col-lg-4 mb-4 mb-lg-0">
-<img data-src="img/23.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="transport gradjevinskog materijala na prikolici" />
-
-<img data-src="img/24.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="utovar gradjevinskog materijala na prikolicu" />
-</div>
-
-<div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-<img data-src="img/33.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy" alt="kamion sa desne strane" />
-
-<img data-src="img/36.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy" alt="kamion sa leve strane" />
-</div>
-
-<div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
-<img data-src="img/35.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy" alt="kamion sa prednje strane" />
-
-<img data-src="img/34.jpg" class="w-100 shadow-1-strong rounded mb-4 lazy"
-    alt="prevoz dva automobila na kamionu" />
-</div>
+<!-- Gallery End -->
 `;
 
-document.getElementById("galleryTemplateId1").innerHTML = gallerySerbian;
-
-setTimeout(function() {
-    
-    document.getElementById("galleryTemplateId2").innerHTML += gallerySecondSerbian;
-    lazyLoadInstance.update();
-
-  }, 2000);
+if (window.innerWidth > 1000) {
+    document.getElementById("galleryTemplateId1").innerHTML = gallerySerbian;
+} else {
+    document.getElementById("galleryTemplateId1").innerHTML = gallerySerbianSmallScreen;
+}
